bc5fb0f42b8df7388c318fc297d7bc029b8d6b85
Ignazio Pollina <i.pollina@itecor.com>
2017-09-25 11:39:31 +0200

    interpolation - eip

--- instances/tf_instance.tf
+++ instances/tf_instance.tf
@@ -2,8 +2,22 @@ provider "aws" {
   region     = "eu-west-1"
 }
 
-resource "aws_instance" "tf_instance" {
+#Public Subnet
+resource "aws_instance" "tf_dck_swam_mngr" {
+  ami           = "ami-bb9a6bc2"
+  instance_type = "t2.micro"
+  subnet_id 	= "subnet-8aed40d1"
+}
+
+#Private Subnet
+resource "aws_instance" "tf_dck_swarm_nd" {
   ami           = "ami-bb9a6bc2"
   instance_type = "t2.micro"
   subnet_id 	= "subnet-8e6405e9"
 }
+
+#Interpolation
+resource "aws_eip" "ip" {
+	instance = "${aws_instance.tf_dck_swam_mngr.id}"
+}
+
