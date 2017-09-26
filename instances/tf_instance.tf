dc99b28e877c7dedb7de9b860cec50d57b844fd1
Ignazio Pollina <i.pollina@itecor.com>
2017-09-26 16:38:43 +0200

    adding some stuff

--- instances/tf_instance.tf
+++ instances/tf_instance.tf
@@ -1,23 +1,78 @@
 provider "aws" {
-  region     = "eu-west-1"
+  region     = "${var.aws_region}"
 }
 
-#Public Subnet
+# Create a VPC
+resource "aws_vpc" "tf_vpc" {
+	cidr_block = "172.16.0.0/16"
+}
+
+# Create an Internet Gateway and assign to VPC
+resource "aws_internet_gateway" "tf_igw_vpc" {
+	vpc_id = "${aws_vpc.tf_vpc.id}"
+}
+
+# Create a subnet 
+resource "aws_subnet" "tf_subnet" {
+	vpc_id = "${aws_vpc.tf_vpc.id}"
+	cidr_block = "172.16.1.0/24"
+	map_public_ip_on_launch = true
+}
+
+# Create a Security Group
+resource "aws_security_group" "tf_sg" {
+	name = "tf_sg"
+	description = "created from terraform"
+	vpc_id = "${aws_vpc.tf_vpc.id}"
+
+	#SSH access
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	#HTTP
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	#Outbound internet access
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
+# Key Pair
+#resource "aws_key_pair" "auth" {
+#	key_name = "${var.key_name}"
+#	key_path = "${file(var.public_key_path)}"
+#}
+
+#Public Instance
 resource "aws_instance" "tf_dck_swam_mngr" {
-  ami           = "ami-bb9a6bc2"
-  instance_type = "t2.micro"
-  subnet_id 	= "subnet-8aed40d1"
-}
+	#connection {
+	#	user = "ec2-user"
+	#}
 
-#Private Subnet
-resource "aws_instance" "tf_dck_swarm_nd" {
-  ami           = "ami-bb9a6bc2"
-  instance_type = "t2.micro"
-  subnet_id 	= "subnet-8e6405e9"
-}
+	ami           			= "${lookup(var.aws_amis,var.aws_region)}"
+	instance_type 			= "t2.micro"
+	#key_name 				= "${aws_key_pair.auth.id}"
+	vpc_security_group_ids	= ["${aws_security_group.tf_sg.id}"]
+	subnet_id 				= "${aws_subnet.tf_subnet.id}"
 
-#Interpolation
-resource "aws_eip" "ip" {
-	instance = "${aws_instance.tf_dck_swam_mngr.id}"
+	provisioner "remote-exec" {
+		inline = [
+			"sudo yum update -y",
+			"sudo yum install -y nginx",
+			"sudo systemctl start nginx",
+		]
+	}
 }
-
---
+++ instances/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+	default = "eu-west-1"
+}
+
+variable "aws_amis" {
+	default = {
+		eu-west-1 = "eu-west-1"
+	}
+}
